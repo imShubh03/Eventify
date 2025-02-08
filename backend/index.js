5af0ebf2dc7d3e95c078bb0a2233354ddd108350
Shubham7476 <shubhamsonake307@gmail.com>
2025-02-08 23:05:48 +0530

    deployment ready

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,12 @@ import userRoutes from "./routes/user.route.js";
 import eventRoutes from "./routes/event.route.js";
 import { Server } from "socket.io";
 import http from "http";
+import path from "path";
 
 dotenv.config();
 connectDB();
+
+const _dirname = path.resolve()
 
 const app = express();
 const server = http.createServer(app);
@@ -19,15 +22,19 @@ app.use(express.json());
 
 app.use("/api/users", userRoutes);
 app.use("/api/events", eventRoutes);
-app.get('/',(req,res)=>{
-    res.send("Hello")
-})
+
+
 io.on("connection", (socket) => {
     console.log("New client connected");
     socket.on("joinEvent", (eventId) => socket.join(eventId));
     socket.on("leaveEvent", (eventId) => socket.leave(eventId));
     socket.on("disconnect", () => console.log("Client disconnected"));
 });
+
+app.use(express.static(path.join(_dirname, "/frontend/dist")))
+app.get('*', (_, res) => {
+    res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"))
+})
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
